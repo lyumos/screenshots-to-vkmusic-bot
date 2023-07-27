19a4454eab6e14dce3c5bfb98ce239e13cd5b07d
lyumos <lyumosvl@yandex.ru>
2023-07-27 19:39:12 +0300

    PD pattern

---
+++ private_data.py
@@ -0,0 +1,7 @@
+# файл со всеми конфиденциальными данными
+vk_login = 'LOGIN'
+vk_password = 'PASSWORD'
+vk_music_link = 'PERSONAL LINK'
+bot_token = 'TOKEN'
+permitted_logins = ['LOGINS']
+imgs_path = 'COMPUTER PATH'
