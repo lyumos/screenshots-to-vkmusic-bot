58295b0c5d6cd4813e626d66d8cad5adaf6c2f95
lyumos <lyumosvl@yandex.ru>
2023-08-12 15:36:02 +0300

    renaming done

--- bot.py
+++ bot.py
@@ -6,25 +6,26 @@ from telegram.ext import CallbackContext
 from telebot import TeleBot, types
 from telebot.types import Message
 from screenshots2songs import sign_in_vk_1, sign_in_vk_2, recognize_text, get_link, crop_img, define_img_type
-from my_private_data import bot_token, imgs_path, users_info
-
-states = {}
+from users_data import bot_token, imgs_path, users_info
 
 bot = telebot.TeleBot(bot_token)
 
 
+# обработчик команды /start
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, 'Бот создан для личного пользования')
     handle_state_one(message)
 
 
+# обработчик ввода логина
 @bot.message_handler(func=lambda message: True, state=1)
 def handle_state_one(message: Message):
     bot.send_message(message.chat.id, 'Введите свой логин')
     bot.register_next_step_handler(message, lambda m: handle_state_two(m))
 
 
+# обработчик логина
 @bot.message_handler(func=lambda message: True, state=2)
 def handle_state_two(message: Message):
     login = message.text
@@ -36,16 +37,16 @@ def handle_state_two(message: Message):
         bot.register_next_step_handler(message, lambda m: handle_state_two(m))
 
 
+# обработчик для инициализации вкхода вк
 @bot.message_handler(func=lambda message: True, state=3)
 def handle_state_three(message, login):
     bot.send_message(message.chat.id, 'Захожу в ВК..')
     driver = sign_in_vk_1(login)
-    # user_id = message.from_user.id
-    # states[user_id] = {'driver': driver}
     bot.send_message(message.chat.id, 'Для продолжения введи код от ВК')
     bot.register_next_step_handler(message, lambda m: handle_state_four(m, driver, login))
 
 
+# обработчик ввода кода
 @bot.message_handler(func=lambda message: True, state=4)
 def handle_state_four(message, driver, login):
     code = message.text
@@ -55,7 +56,8 @@ def handle_state_four(message, driver, login):
     bot.register_next_step_handler(message, lambda m: handle_state_five(m, driver, login))
 
 
-@bot.message_handler(content_types=['photo'], state = 5)
+# обработчик скриншотов
+@bot.message_handler(content_types=['photo'], state=5)
 def handle_state_five(message, driver, login):
     bot.send_message(message.chat.id, 'Скриншот принят')
     image_id = message.photo[-1].file_id
@@ -87,7 +89,9 @@ def handle_state_five(message, driver, login):
     bot.send_message(message.chat.id, 'Есть ли есть еще скриншоты?', reply_markup=keyboard)
     bot.register_next_step_handler(message, lambda m: handle_state_six(m, driver, login))
 
-@bot.message_handler(content_types=['photo'], state = 6)
+
+# обработчик выбора пользователя
+@bot.message_handler(content_types=['photo'], state=6)
 def handle_state_six(message, driver, login):
     answer = message.text
     if message.content_type == 'text':
